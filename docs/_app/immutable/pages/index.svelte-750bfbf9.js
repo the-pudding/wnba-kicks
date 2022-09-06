5adf8159177618cb5fa25e3bb67efd411bca2112
Jan Diehm <jadiehm@gmail.com>
2022-09-06 13:32:54 -0500

    update github pages

---
+++ docs/_app/immutable/pages/index.svelte-750bfbf9.js
@@ -0,0 +1,1 @@
+import"../chunks/index-58d72424.js";import{R as i}from"../chunks/index-a0c3e7a3.js";import"../chunks/transform-ffa743a8.js";import"../chunks/preload-helper-7f45cd29.js";export{i as default};
